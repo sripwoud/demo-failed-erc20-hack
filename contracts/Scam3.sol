eb7603367f8b853de086198ff73dacc9ff0e0361
3pwd <3pwd@tuta.io>
2022-05-31 14:18:30 +0200

    test sending eth to non payable func

--- contracts/Scam3.sol
+++ contracts/Scam3.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "hardhat/console.sol";
 
 contract Scam3 is ERC20 {
     constructor() ERC20("Baby Usdc", "BBUSDC") {}
